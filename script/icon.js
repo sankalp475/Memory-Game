89ef2b90705c3d074159ef369f59ed866dabc4b5
sankalp kumar <65161219+sankalp475@users.noreply.github.com>
2022-09-17 14:11:02 +0530

    new commit after changes

---
+++ script/icon.js
@@ -0,0 +1,98 @@
+
+export default {
+	_icon_1: {
+		_SvgCode: () => `
+		    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+			    <path fill="none" d="M0 0h24v24H0z"/>
+				<path d="M11.5 2a9.5 9.5 0 0 1 9.5 9.5 9.46 9.46 0 0 1-1.003 4.254l2.463 2.464a1 1 0 0 1 0 1.414l-2.828 2.828a1 1 0 0 1-1.414 0l-2.464-2.463A9.46 9.46 0 0 1 11.5 21a9.5 9.5 0 0 1 0-19zm5.303 13.388l-1.414 1.414 3.536 3.535 1.414-1.414-3.536-3.535zm1.864-6.105l-9.384 9.384c.7.216 1.445.333 2.217.333a7.48 7.48 0 0 0 2.74-.516l-.972-.974a1 1 0 0 1 0-1.414l2.828-2.828a1 1 0 0 1 1.414 0l.974.972A7.48 7.48 0 0 0 19 11.5c0-.772-.117-1.516-.333-2.217zM11.5 4a7.5 7.5 0 0 0-4.136 13.757L17.757 7.364A7.493 7.493 0 0 0 11.5 4z" fill="#ff3860"/>
+		    </svg>
+		`,
+		type: 'ping-pong',
+	},
+	_icon_2: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ball-football" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<circle cx="12" cy="12" r="9" />
+				<path d="M12 7l4.76 3.45l-1.76 5.55h-6l-1.76 -5.55z" />
+				<path d="M12 7v-4m3 13l2.5 3m-.74 -8.55l3.74 -1.45m-11.44 7.05l-2.56 2.95m.74 -8.55l-3.74 -1.45" />
+			</svg>
+		`,
+		type: 'football',
+	},
+	_icon_3: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ball-american-football" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<line x1="15" y1="9" x2="9" y2="15" />
+				<line x1="10" y1="12" x2="12" y2="14" />
+				<line x1="12" y1="10" x2="14" y2="12" />
+				<path d="M8 21a5 5 0 0 0 -5 -5" />
+				<path d="M16 3c-7.18 0 -13 5.82 -13 13a5 5 0 0 0 5 5c7.18 0 13 -5.82 13 -13a5 5 0 0 0 -5 -5" />
+				<path d="M16 3a5 5 0 0 0 5 5" />
+			  </svg>
+		`,
+		type: 'ball-american-football',
+	},
+	_icon_4: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bike" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+				<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+				<circle cx="5" cy="18" r="3" />
+				<circle cx="19" cy="18" r="3" />
+				<polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />
+				<circle cx="17" cy="5" r="1" />
+			  </svg>
+		`,
+		type: 'bike',
+	},
+	_icon_5: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-jump-rope" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M6 14v-6a3 3 0 1 1 6 0v8a3 3 0 0 0 6 0v-6" />
+			<rect x="16" y="3" width="4" height="7" rx="2" />
+			<rect x="4" y="14" width="4" height="7" rx="2" />
+		  </svg>
+		`,
+		type: 'jump-rope',
+	},
+	_icon_6: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-swimming" width="44" height="44"  viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<circle cx="16" cy="9" r="1" />
+			<path d="M6 11l4 -2l3.5 3l-1.5 2" />
+			<path d="M3 16.75a2.4 2.4 0 0 0 1 .25a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 1 -.25" />
+		  </svg>
+		`,
+		type: 'swimming',
+	},
+	_icon_7: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-soccer-field" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<circle cx="12" cy="12" r="3" />
+			<path d="M3 9h3v6h-3z" />
+			<path d="M18 9h3v6h-3z" />
+			<rect x="3" y="5" width="18" height="14" rx="2" />
+			<line x1="12" y1="5" x2="12" y2="19" />
+		  </svg>
+		`,
+		type: 'soccer-field',
+	},
+	_icon_8: {
+		_SvgCode: () => `
+			<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pool" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ff3860" fill="none" stroke-linecap="round" stroke-linejoin="round">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M2 20a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1" />
+			<path d="M2 16a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1a2.4 2.4 0 0 1 2 -1a2.4 2.4 0 0 1 2 1a2.4 2.4 0 0 0 2 1a2.4 2.4 0 0 0 2 -1" />
+			<path d="M15 12v-7.5a1.5 1.5 0 0 1 3 0" />
+			<path d="M9 12v-7.5a1.5 1.5 0 0 0 -3 0" />
+			<line x1="15" y1="5" x2="9" y2="5" />
+			<line x1="9" y1="10" x2="15" y2="10" />
+		    </svg>
+	    `,
+		type: 'pool',
+	},
+}
---
+++ script/memoryCard.js
@@ -0,0 +1,277 @@
+import icon from "./icon.js";
+const audio = {
+	cardFlipSound: new Audio("./assets/audio/Assets_Audio_flip.wav"),
+	Victory_Sound: new Audio("./assets/audio/Assets_Audio_victory.wav"),
+	gameOver_sound: new Audio(
+		"./assets/audio/mixkit-wrong-answer-fail-notification-946.wav"
+	),
+};
+const scoreCount = document.querySelector("span[data-flip-time]");
+const flipCounter = document.querySelector("span[data-flip-move]");
+const displayScreen = document.querySelector(".game-popup-bord");
+const refreshbtn = document.querySelector(".refresh-btn");
+displayScreen.dataset.key = localStorage.getItem("key");
+const card = document.querySelectorAll(".card");
+window.onload = () => {
+	displayScreen.dataset.key = localStorage.getItem("key");
+};
+const {
+	_icon_1,
+	_icon_2,
+	_icon_3,
+	_icon_4,
+	_icon_5,
+	_icon_6,
+	_icon_7,
+	_icon_8,
+} = icon;
+const cardImageArray = [
+	{ src: _icon_1._SvgCode(), name: _icon_1.type },
+	{ src: _icon_1._SvgCode(), name: _icon_1.type },
+	{ src: _icon_2._SvgCode(), name: _icon_2.type },
+	{ src: _icon_2._SvgCode(), name: _icon_2.type },
+	{ src: _icon_3._SvgCode(), name: _icon_3.type },
+	{ src: _icon_3._SvgCode(), name: _icon_3.type },
+	{ src: _icon_4._SvgCode(), name: _icon_4.type },
+	{ src: _icon_4._SvgCode(), name: _icon_4.type },
+	{ src: _icon_5._SvgCode(), name: _icon_5.type },
+	{ src: _icon_5._SvgCode(), name: _icon_5.type },
+	{ src: _icon_6._SvgCode(), name: _icon_6.type },
+	{ src: _icon_6._SvgCode(), name: _icon_6.type },
+	{ src: _icon_7._SvgCode(), name: _icon_7.type },
+	{ src: _icon_7._SvgCode(), name: _icon_7.type },
+	{ src: _icon_8._SvgCode(), name: _icon_8.type },
+	{ src: _icon_8._SvgCode(), name: _icon_8.type },
+];
+
+const state = {
+	remaningTime: 100,
+	isTimerStart: false,
+	isTimeEnded: false,
+	isGameStarted: false,
+	winCount: 0,
+	flipCount: 0,
+	_TOTLE_MOVE: 16 / 2,
+	refresh: false,
+	popup_toggle: () => {
+		//* for game start popup
+		if (state.remaningTime == 100 && state.isTimerStart == false) {
+			localStorage.setItem("key", "GS");
+			displayScreen.dataset.key = localStorage.getItem("key");
+		}
+		//* for game over popup
+		if (
+			state.remaningTime <= 0 &&
+			state.isTimeEnded == true &&
+			state.winCount < state._TOTLE_MOVE
+		) {
+			localStorage.setItem("key", "GE");
+			displayScreen.dataset.key = localStorage.getItem("key");
+			audio.gameOver_sound.play();
+		}
+		//* for game Win popup
+		if (
+			state.remaningTime >= 0 &&
+			state.remaningTime !== 100 &&
+			state.isTimeEnded == false &&
+			state.winCount == state._TOTLE_MOVE
+		) {
+			localStorage.setItem("key", "GW");
+			displayScreen.dataset.key = localStorage.getItem("key");
+			audio.Victory_Sound.play();
+		}
+		//* on game after started function
+		if (
+			state.remaningTime < 100 &&
+			state.isGameStarted == true &&
+			state.isTimeEnded == false &&
+			state.isTimerStart == true
+		) {
+			localStorage.setItem("key", "null");
+			displayScreen.dataset.key = localStorage.getItem("key");
+		}
+	},
+	_reset: (elmement) => {
+		setTimeout(() => {
+			state.flipCount = 0;
+			state.remaningTime = 100;
+			state.winCount = 0;
+			state.isTimerStart = false
+			state.isTimeEnded = false,
+				state.isGameStarted = false,
+				elmement.innerHTML = state.flipCount;
+			let cards = document.querySelectorAll('.card');
+			let randomCard = ranDomiser();
+			cards.forEach((card, index) => {
+				setTimeout(() => {
+					card.querySelectorAll(".front")[0].innerHTML = randomCard[index].src;
+					card.dataset.name = randomCard[index].name;
+				}, 1000);
+				card.dataset.matchedcard = false
+			})
+		}, 500);
+	}
+};
+
+window.startgame = () => {
+	let displayScreen = document.querySelector(".game-popup-bord");
+	if (displayScreen.dataset.key == "GS" || displayScreen.dataset.key == "Gs") {
+		displayScreen.dataset.key = "null";
+		state._reset(flipCounter)
+	}
+	if (displayScreen.dataset.key == "GE" || displayScreen.dataset.key == "Ge") {
+		displayScreen.dataset.key = "null";
+		state._reset(flipCounter)
+	}
+	if (displayScreen.dataset.key == "GW" || displayScreen.dataset.key == "Gw") {
+		displayScreen.dataset.key = "null";
+		card.forEach((card) => {
+			let frontFace = card.querySelector(".front");
+			let backFace = card.querySelector(".back");
+			backFace.classList.remove("back");
+			backFace.classList.add("front");
+			frontFace.classList.remove("front");
+			frontFace.classList.add("back");
+			card.classList.remove("matched");
+			setTimeout(() => {
+				card.classList.remove("active-match");
+			}, 600);
+		});
+	}
+	updateTimer();
+	audio.cardFlipSound.play();
+};
+function updateTimer() {
+	let displayScreen = document.querySelector(".game-popup-bord");
+	const i = setInterval(() => {
+		state.remaningTime--;
+		scoreCount.innerHTML = state.remaningTime;
+		if (state.remaningTime < 100 || state.remaningTime >= 1) {
+			state.isGameStarted = true;
+			state.isTimeEnded = false;
+			state.isTimerStart = true;
+		}
+		if (state.remaningTime <= 0) {
+			clearInterval(i);
+			state.isTimerStart = false;
+			state.isTimeEnded = true;
+			state.isGameStarted = false;
+			state.popup_toggle();
+			document.querySelector(".game-end .score-data").innerHTML =
+				state.flipCount;
+			document.querySelector(".game-end .Flips-data").innerHTML =
+				state.winCount * 2;
+			displayScreen.dataset.key = 'GE'
+			audio.gameOver_sound.play()
+		}
+		// state.popup_toggle()
+		if (
+			displayScreen.dataset.key == "GW" ||
+			displayScreen.dataset.key == "Gw"
+		) {
+			clearInterval(i);
+			state._reset(flipCounter)
+		}
+	    console.log(`%cMix-or-Match game %c`,"font-size:14px; color: #ff3860;background:black;padding:8px;padding-right:0px", "font-size:14px;color:orange;font-weight:bold;background:black;padding:8px;padding-left:0px")
+	}, 1000);
+}
+// console.log(`%cMix-or-Match game %c`,"font-size:14px; color: #ff3860;background:black;padding:8px;padding-right:0px", "font-size:14px;color:orange;font-weight:bold;background:black;padding:8px;padding-left:0px")
+
+function ranDomiser() {
+	let cardData = cardImageArray;
+	cardData.sort(() => Math.random() - 0.5);
+	return cardData;
+}
+function _HandleCardEvent(card, index, randomCard) {
+	card.querySelectorAll(".front")[0].innerHTML = randomCard[index].src;
+	card.dataset.name = randomCard[index].name;
+	card.addEventListener("click", (event) => {
+		if (!card.classList.contains("matched")) {
+			card.classList.add("active-match");
+			card.setAttribute('data-cardfliped', true);
+			audio.cardFlipSound.play();
+			flipCounter.innerHTML = state.flipCount++;
+		}
+		const flipedcard = document.querySelectorAll(".active-match");
+		if (flipedcard.length > 2) {
+			flipedcard[2].classList.remove("active-match");
+		}
+		checkForMatch(index, flipedcard);
+	});
+}
+
+function checkForMatch(index, flipedcard) {
+	// console.log(card[index])
+	if (flipedcard.length == 2) {
+		if (flipedcard[0].dataset.name == flipedcard[1].dataset.name) {
+			flipedcard[0].dataset.matchedcard = true
+			flipedcard[1].dataset.matchedcard = true
+			// console.log(" %cmatched%c ","font-size:14px; color: #4fdee5;background:black;padding:8px;padding-right:0px", "font-size:14px;color:orange;font-weight:bold;background:black;padding:8px;padding-left:0px")
+			_score_check();
+			flipedcard.forEach((card) => {
+				let frontFace = card.querySelector(".front");
+				let backFace = card.querySelector(".back");
+				let temp = backFace.innerHTML;
+				backFace.innerHTML = frontFace.innerHTML;
+				frontFace.innerHTML = temp;
+				card.classList.add("matched");
+				setTimeout(() => {
+					card.classList.remove("active-match");
+				}, 600);
+			});
+		} else {
+			flipedcard[0].dataset.matchedcard = false
+			flipedcard[1].dataset.matchedcard = false
+			// document.querySelector('.')
+			// console.log(" %cun-matched%c ","font-size:14px; color: #ff3860;background:black;padding:8px;padding-right:0px", "font-size:14px;color:orange;font-weight:bold;background:black;padding:8px;padding-left:0px")
+			flipedcard.forEach((card) => {
+				setTimeout(() => {
+					card.classList.remove("active-match");
+				}, 600);
+			});
+		}
+	}
+}
+
+function _score_check() {
+	state.winCount += 1;
+	if (state.winCount == state._TOTLE_MOVE) {
+		state.popup_toggle();
+		localStorage.setItem("key", "GW");
+		displayScreen.dataset.key = localStorage.getItem("key");
+		document.querySelector(".game-win .score-data").innerHTML = state.flipCount;
+		document.querySelector(".game-win .Flips-data").innerHTML =
+			state._TOTLE_MOVE * 2;
+	}
+}
+refreshbtn.addEventListener('click', (event) => {
+	let _matchedcard = document.querySelectorAll('div[data-matchedcard="true"]')
+	state.refresh = true;
+	if (_matchedcard.length > 0) {
+		_matchedcard.forEach((card, index) => {
+			let frontcard = card.querySelector('.front')
+			let backcard = card.querySelector('.back')
+			frontcard.classList.add('back')
+			frontcard.classList.remove('front')
+			backcard.classList.add('front')
+			backcard.classList.remove('back')
+			card.classList.remove('matched')
+			setTimeout(() => {
+				card.dataset.matchedcard = false
+			}, 800)
+			state._reset(flipCounter)
+		})
+	} else {
+		_matchedcard.forEach((card, index) => {
+			card.dataset.matchedcard = false
+		})
+
+	}
+})
+const start = (() => {
+	state.popup_toggle();
+	let randomCard = ranDomiser();
+	card.forEach((card, index) => {
+		_HandleCardEvent(card, index, randomCard);
+	});
+})();
